6ca5b5b58289696eb644d43814469c4cfc281815
Gabriel fernandes <Gabrielfernandesfotografias@outlook.com>
2021-07-07 10:03:32 -0300

    add comentarios

--- public/js/toggle.js
+++ public/js/toggle.js
@@ -1,8 +1,9 @@
-
+// peguei o botao e transformei em uma variavel
 let navigation = document.querySelector("#navbarSideCollapse");
-
+// peguei o container que eu queria esconder e transformei em uma variavel
 let navbarsExampleDefault = document.querySelector("#navbarsExampleDefault");
-
+//peguei a variavel do botão e o evento do click em seguida coloquei uma classe dentro do container 
+//que eu queria esconder. fui no css e apenas com nome da classe e um display none resolvi
 navigation.onclick = function () {
   navbarsExampleDefault.classList.toggle("sidebarhide")
 
